63011a40e30d90b64446ca0180d8d4b398de5d15
Steve Brownlee <chortlehoort@gmail.com>
2019-05-24 12:42:14 -0500

    Cohort list complete

---
+++ StudentExercisesMVC/Controllers/CohortsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentExercisesMVC.Repositories;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class CohortsController : Controller
+    {
+        // GET: Cohorts
+        public ActionResult Index()
+        {
+            var cohorts = CohortRepository.GetCohorts();
+            return View(cohorts);
+        }
+
+        // GET: Cohorts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cohorts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cohorts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cohorts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- StudentExercisesMVC/Controllers/StudentsController.cs
+++ StudentExercisesMVC/Controllers/StudentsController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StudentExercisesMVC.Models.ViewModels;
 using StudentExercisesMVC.Repositories;
